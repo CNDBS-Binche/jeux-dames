c8c9fff4e467eabd25ec808ebd3d8baa2e9b8534
MAX <maximepetit886@gmail.com>
2026-04-17 09:33:16 +0200

    affichage du plateau

---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="style.css">
+<title>Damier</title>
+</head>
+
+<body>
+
+<table>
+
+  <tbody>
+    <!-- Ligne 1 -->
+    <tr>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      
+    </tr>
+
+    <!-- Ligne 2 -->
+    <tr>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+    </tr>
+
+    <!-- Répète le motif -->
+    <!-- Ligne 3 -->
+    <tr>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+    </tr>
+
+    <!-- Ligne 4 -->
+    <tr>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+    </tr>
+
+    <!-- Ligne 5 -->
+    <tr>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+    </tr>
+
+    <!-- Ligne 6 -->
+    <tr>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+    </tr>
+
+    <!-- Ligne 7 -->
+    <tr>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+    </tr>
+
+    <!-- Ligne 8 -->
+    <tr>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+    </tr>
+
+    <!-- Ligne 9 -->
+    <tr>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+    </tr>
+
+    <!-- Ligne 10 -->
+    <tr>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+      <td class="black"></td>
+      <td class="white"></td>
+    </tr>
+  </tbody>
+</table>
+
+</body>
+</html>
