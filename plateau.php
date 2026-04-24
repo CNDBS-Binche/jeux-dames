90111b3cfd01a6240d17123f2b46de396182bad3
Maxlamenace-dev <maxime.petit@eleve.cnbds.be>
2026-04-24 10:30:40 +0200

    ajout d'un système php pour le damier

--- plateau.php
+++ plateau.php
@@ -25,94 +25,48 @@ if (!isset($_SESSION['user_id'])) {
       <td class="coord">F</td><td class="coord">G</td><td class="coord">H</td><td class="coord">I</td><td class="coord">J</td>
       <td class="coord"></td> </tr>
   </thead>
-  <tbody>
-    <tr>
-      <td class="coord">1</td>
-      <td class="white"></td><td class="black"><div class="pion noir"></div></td>
-      <td class="white"></td><td class="black"><div class="pion noir"></div></td>
-      <td class="white"></td><td class="black"><div class="pion noir"></div></td>
-      <td class="white"></td><td class="black"><div class="pion noir"></div></td>
-      <td class="white"></td><td class="black"><div class="pion noir"></div></td>
-      <td class="coord">1</td>
-    </tr>
-    <tr>
-      <td class="coord">2</td>
-      <td class="black"><div class="pion noir"></div></td><td class="white"></td>
-      <td class="black"><div class="pion noir"></div></td><td class="white"></td>
-      <td class="black"><div class="pion noir"></div></td><td class="white"></td>
-      <td class="black"><div class="pion noir"></div></td><td class="white"></td>
-      <td class="black"><div class="pion noir"></div></td><td class="white"></td>
-      <td class="coord">2</td>
-    </tr>
-    <tr>
-      <td class="coord">3</td>
-      <td class="white"></td><td class="black"><div class="pion noir"></div></td>
-      <td class="white"></td><td class="black"><div class="pion noir"></div></td>
-      <td class="white"></td><td class="black"><div class="pion noir"></div></td>
-      <td class="white"></td><td class="black"><div class="pion noir"></div></td>
-      <td class="white"></td><td class="black"><div class="pion noir"></div></td>
-      <td class="coord">3</td>
-    </tr>
-    <tr>
-      <td class="coord">4</td>
-      <td class="black"><div class="pion noir"></div></td><td class="white"></td>
-      <td class="black"><div class="pion noir"></div></td><td class="white"></td>
-      <td class="black"><div class="pion noir"></div></td><td class="white"></td>
-      <td class="black"><div class="pion noir"></div></td><td class="white"></td>
-      <td class="black"><div class="pion noir"></div></td><td class="white"></td>
-      <td class="coord">4</td>
-    </tr>
+<tbody>
+    <?php
+    // 1. Initialisation du plateau en mémoire (0=vide, 1=blanc, 2=noir)
+    // Dans un vrai projet, ceci viendra d'une base de données ou d'une session
+    $plateau = [];
+    for ($l = 1; $l <= 10; $l++) {
+        for ($c = 1; $c <= 10; $c++) {
+            $plateau[$l][$c] = 0; // Par défaut vide
+            if (($l + $c) % 2 != 0) { // Uniquement sur les cases noires
+                if ($l <= 4) $plateau[$l][$c] = 2; // Pions Noirs
+                if ($l >= 7) $plateau[$l][$c] = 1; // Pions Blancs
+            }
+        }
+    }
 
-    <tr>
-      <td class="coord">5</td>
-      <td class="white"></td><td class="black"></td><td class="white"></td><td class="black"></td><td class="white"></td>
-      <td class="black"></td><td class="white"></td><td class="black"></td><td class="white"></td><td class="black"></td>
-      <td class="coord">5</td>
-    </tr>
-    <tr>
-      <td class="coord">6</td>
-      <td class="black"></td><td class="white"></td><td class="black"></td><td class="white"></td><td class="black"></td>
-      <td class="white"></td><td class="black"></td><td class="white"></td><td class="black"></td><td class="white"></td>
-      <td class="coord">6</td>
-    </tr>
+    // 2. Génération automatique du HTML
+    for ($ligne = 1; $ligne <= 10; $ligne++) {
+        echo "<tr>";
+        echo "<td class='coord'>$ligne</td>"; // Coordonnée gauche
 
-    <tr>
-      <td class="coord">7</td>
-      <td class="white"></td><td class="black"><div class="pion blanc"></div></td>
-      <td class="white"></td><td class="black"><div class="pion blanc"></div></td>
-      <td class="white"></td><td class="black"><div class="pion blanc"></div></td>
-      <td class="white"></td><td class="black"><div class="pion blanc"></div></td>
-      <td class="white"></td><td class="black"><div class="pion blanc"></div></td>
-      <td class="coord">7</td>
-    </tr>
-    <tr>
-      <td class="coord">8</td>
-      <td class="black"><div class="pion blanc"></div></td><td class="white"></td>
-      <td class="black"><div class="pion blanc"></div></td><td class="white"></td>
-      <td class="black"><div class="pion blanc"></div></td><td class="white"></td>
-      <td class="black"><div class="pion blanc"></div></td><td class="white"></td>
-      <td class="black"><div class="pion blanc"></div></td><td class="white"></td>
-      <td class="coord">8</td>
-    </tr>
-    <tr>
-      <td class="coord">9</td>
-      <td class="white"></td><td class="black"><div class="pion blanc"></div></td>
-      <td class="white"></td><td class="black"><div class="pion blanc"></div></td>
-      <td class="white"></td><td class="black"><div class="pion blanc"></div></td>
-      <td class="white"></td><td class="black"><div class="pion blanc"></div></td>
-      <td class="white"></td><td class="black"><div class="pion blanc"></div></td>
-      <td class="coord">9</td>
-    </tr>
-    <tr>
-      <td class="coord">10</td>
-      <td class="black"><div class="pion blanc"></div></td><td class="white"></td>
-      <td class="black"><div class="pion blanc"></div></td><td class="white"></td>
-      <td class="black"><div class="pion blanc"></div></td><td class="white"></td>
-      <td class="black"><div class="pion blanc"></div></td><td class="white"></td>
-      <td class="black"><div class="pion blanc"></div></td><td class="white"></td>
-      <td class="coord">10</td>
-    </tr>
-  </tbody>
+        for ($col = 1; $col <= 10; $col++) {
+            $typeCase = ($ligne + $col) % 2 == 0 ? 'white' : 'black';
+            $contenuCase = "";
+
+            // On place le pion si la case n'est pas vide dans notre tableau
+            if ($plateau[$ligne][$col] == 1) {
+                $contenuCase = '<div class="pion blanc"></div>';
+            } elseif ($plateau[$ligne][$col] == 2) {
+                $contenuCase = '<div class="pion noir"></div>';
+            }
+
+            // On ajoute des attributs data- pour que le JS puisse identifier la case
+            echo "<td class='$typeCase' data-ligne='$ligne' data-col='$col'>";
+            echo $contenuCase;
+            echo "</td>";
+        }
+
+        echo "<td class='coord'>$ligne</td>"; // Coordonnée droite
+        echo "</tr>";
+    }
+    ?>
+</tbody>
   <tfoot>
     <tr>
       <td class="coord"></td>
